9b5e57e24a27a6b1934a162115879d04ee19d8e4
SC-IDO <ido@sitecore.net>
2019-04-03 14:02:51 +0300

    Empty assembly as a patch marker

---
+++ src/Sitecore.Support.316852/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.316852")]
+[assembly: AssemblyProduct("Sitecore.Support.316852")]
+[assembly: ComVisible(false)]
